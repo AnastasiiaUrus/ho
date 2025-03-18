a04904a4662250e81316e28c09e247cf90c09355
Анастасія <anastasia@MacBook-Air-Anastasia.local>
2025-03-18 23:01:44 +0200

    Add is_power_of_5 function and its usage

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from utils import factorial, is_prime
+from utils import factorial, is_prime, is_power_of_5
 
 def main():
     number = 5
     print(f"Factorial of {number} is {factorial(number)}")
     print(f"Is {number} prime? {is_prime(number)}")
+    print(f"Is {number} a power of 5? {is_power_of_5(number)}")
 
 if __name__ == "__main__":
     main()
