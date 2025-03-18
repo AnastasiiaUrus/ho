87a2316865e3331931ed4aa67ce4f79e84f70195
Анастасія <anastasia@MacBook-Air-Anastasia.local>
2025-03-18 23:02:56 +0200

    Add is_power_of_2 function and its usage

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from utils import factorial, is_prime
+from utils import factorial, is_prime, is_power_of_2
 
 def main():
     number = 5
     print(f"Factorial of {number} is {factorial(number)}")
     print(f"Is {number} prime? {is_prime(number)}")
+    print(f"Is {number} a power of 2? {is_power_of_2(number)}")
 
 if __name__ == "__main__":
     main()
